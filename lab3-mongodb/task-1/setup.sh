1630d84302003c1c1f675d8d6e4fab710358fb64
Andrii Yerko <aerko12@gmail.com>
2023-01-05 00:16:52 +0200

    [lab3] add task2

--- lab3-mongodb/task-1/setup.sh
+++ lab3-mongodb/task-1/setup.sh
@@ -8,7 +8,7 @@ docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
 docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
 docker-compose exec shard03-a sh -c "mongosh < /scripts/init-shard03.js"
 
-sleep 3
+sleep 10
 
 docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
 
